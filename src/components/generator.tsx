1d1e67606b5970eec439c3c0452a928525bf7eef
vorob <vorobyov_egor@bk.ru>
2019-11-23 20:14:19 +0300

    add file

--- src/components/generator.tsx
+++ src/components/generator.tsx
@@ -1,43 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import names from '../assets/name.json'
 import surnames from '../assets/secondname.json'
 import ages from '../assets/age.json'
 import streets from '../assets/streets.json'
-import getRandom from "./random";
+import getRandom from "./Random/random";
+import People from "./People/people";
 
-class App extends React.Component{
+class Generator extends Component {
     state = {
         people: [
             {
-                name: "NAME",
-                surname: "SURNAME",
-                street: "STREET",
-                age: "AGE"
+                name: "Egor",
+                surname: "Vorobyov",
+                age: "19",
+                street: "Jakubova"
             }
-        ],
+        ]
     };
 
     getRandomName = () => getRandom(names);
-    getRandomSurname = () => getRandom(surnames);
+    getRandomSurnames = () => getRandom(surnames);
     getRandomAge = () => getRandom(ages);
     getRandomStreet = () => getRandom(streets);
 
     createPeople = () => {
         this.setState({
-            people: [
-                ...this.state.people,
-                {
-                    name: this.getRandomName(),
-                    surname: this.getRandomSurname(),
-                    age: this.getRandomAge(),
-                    street: this.getRandomStreet(),
-                }
-            ]
+            name: this.getRandomName(),
+            surname: this.getRandomSurnames(),
+            age: this.getRandomSurnames(),
+            street: this.getRandomStreet()
         })
     };
 
-    handleClicked = () => this.createPeople();
+    handleClick = () => this.createPeople();
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
 
@@ -45,27 +41,13 @@ class App extends React.Component{
             <div>
                 <People name={people.name} surname={people.surname} street={people.street} age={people.age}/>
             </div> ));
+
         return (
             <div>
-                <div className="header">
-                    Radnomize
-                    <button className="button" onClick={this.handleClicked}>click</button>
+                <div>
+                    <button onClick={this.handleClick}></button>
                 </div>
-                <ul style={{listStyleType:"none"}}>
-                    {people}
-                </ul>
             </div>
         );
     }
 }
-
-// @ts-ignore
-const People = ({name, surname, age, street}) => (
-    <div className="people">
-        <div>{surname + " " + name}</div>
-        <div>Age:{age}</div>
-        <div>Street:{street}</div>
-    </div>
-);
-
-export default App;
